1421891690c72aac5081d8011ecfbe088b8614ae
jai08 <55553176+Jayakumar-dev@users.noreply.github.com>
2023-07-01 21:39:43 +0530

    Update mainPage.py

--- mainPage.py
+++ mainPage.py
@@ -5,9 +5,6 @@ from tkinter import filedialog
 from backend import run_all
 import os
 
-os.system('Xvfb :1 -screen 0 1600x1200x16  &')    # create virtual display with size 1600x1200 and 16 bit color. Color can be changed to 24 or 8
-os.environ['DISPLAY']=':1.0'    # tell X clients to use our virtual DISPLAY :1.0.
-
 # Set up tkinter
 root = tk.Tk()
 root.withdraw()
